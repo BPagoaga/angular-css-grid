73bcddff857fe85e13edb9d7e51c5feebac344fe
Bernard Pagoaga <bernard.pagoaga@webdealauto.com>
2018-07-03 09:51:35 +0200

    [INIT]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HomeComponent } from "./components/home/home.component";
+import { HeaderComponent } from "./app-shell/header/header.component";
+import { FooterComponent } from "./app-shell/footer/footer.component";
+import { NavbarComponent } from "./app-shell/navbar/navbar.component";
 
-import { AppComponent } from './app.component';
-
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "dashboard", component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
